b1430c009cec38b982f9746f43bc4754344a8075
Daniel Amrine <daniel.amrine@csuglobal.edu>
2019-07-29 10:46:01 -0600

    Update MS510_CTmd2_25jul19.R
    Used to analyze Cereals.csv from https://perso.telecom-paristech.fr/eagan/class/igr204/datasets

--- MS510_CTmd2_25jul19.R
+++ MS510_CTmd2_25jul19.R
@@ -23,5 +23,42 @@ data.frame(mean=sapply(cer_qval,mean,na.rm=TRUE),
             length=sapply(cer_qval,length),
             mis.val=sapply(cer_qval, function(x)
             sum(length(which(is.na(x)))))) 
+                           
+generate a simple histogram for each variable
+hist(cer_qval$calories, xlab = "Calories")
+#use Par to split the panels into columns
+par(mfcol = c(4,4))
+# All 13 histograms
+hist(cer_qval$calories, xlab = "Calories")
+hist(cer_qval$protein, xlab = "Protein")
+hist(cer_qval$fat, xlab = "Fat")
+hist(cer_qval$sodium, xlab = "Sodium")
+hist(cer_qval$fiber, xlab = "Fiber")
+hist(cer_qval$carbo, xlab = "Carbohydrates")
+hist(cer_qval$sugars, xlab = "Sugars")
+hist(cer_qval$potass, xlab = "Potassium")
+hist(cer_qval$vitamins, xlab = "Vitamins")
+hist(cer_qval$shelf, xlab = "Shelf Height")
+hist(cer_qval$weight, xlab = "Weight")
+hist(cer_qval$cups, xlab = "Cups")
+hist(cer_qval$rating, xlab = "Customer Rating")
+
+#Generate a box plot of consumer rating 
+#vs Shelf height
+par(mfcol = c(1,2))
+hist(cer_qval$rating, xlab = "Customer Rating")
+boxplot(cereals$rating ~ cereals$shelf, xlab = "Shelf Height",
+        ylab = "Customer Rating")
+     
+
+#Option 1
+par(mfcol = c(1,2))
+hist(cereals$calories, xlab = "Calories")
+boxplot(cereals$calories ~ cereals$type, xlab = "Cold or Hot",
+        ylab = "Calories")                          
+                           
+                           
+                           
+ 
             
             
